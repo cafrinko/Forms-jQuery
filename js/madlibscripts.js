e82dec7fcaa7c5fe7a64a837fa1767fc09ffaf1e
catherine karra and gigi ditz <gigi@>
2016-03-23 11:22:15 -0700

    Create personal letter

---
+++ js/madlibscripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person").val();
+
+    $(".person").text(person1Input);
+
+    $("#letter").show();
+
+    event.preventDefault();
+  });
+});
