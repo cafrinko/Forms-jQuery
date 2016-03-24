50b7169d9b014b3e65c440c695ef65efbed810dc
catherine karra and gigi ditz <gigi@>
2016-03-23 17:17:18 -0700

    Put .toUpperCase in js file instead of css

--- js/madlibscripts.js
+++ js/madlibscripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var person1Input = $("input#person").val();
 
-    $(".person").text(person1Input);
+    $(".person").text(person1Input.toUpperCase());
 
 
     $("#letter").show();
